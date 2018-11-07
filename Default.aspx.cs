e4db65196d897af3fda3504b2e350644e266db30
Esteban <esteban.smits@nttdata.com>
2018-11-07 13:08:12 -0600

    initial commit

---
+++ Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public string compName;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.compName = System.Environment.GetEnvironmentVariables()["COMPUTERNAME"].ToString();
+
+    }
+}
